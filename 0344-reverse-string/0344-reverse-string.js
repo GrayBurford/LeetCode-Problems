f1fb6a60f3583832a9fb22f0d7fb25efea28f2bc
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-01-19 12:42:40 -0500

    Time: 97 ms (75.35%), Space: 49.5 MB (32.63%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,23 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+// var reverseString = function(s) {
+//     return s.reverse()
+// };
+
+function reverseString (s) {
+    let start = 0;
+    let end = s.length - 1;
+    
+    while (start < end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        
+        start++;
+        end--;
+    }
+    
+    return s;
+}
