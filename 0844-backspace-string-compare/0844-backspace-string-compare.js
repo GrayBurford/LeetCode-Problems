c90234c89c5de2ec05c780a391b106c01190afa3
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-02-13 11:58:54 -0500

    Time: 56 ms (95.82%), Space: 42.7 MB (44.10%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -20,3 +20,9 @@ function processString (string) {
     
     return stack.join('');
 }
+
+// Recognize LIFO (last in; first out) pattern
+// When we encounter a '#', we will be removing the last entry to the stack
+// Build helper function to convert raw input string to process hash symbols
+// Initialize stack; loop over string; if char is not #, push -- else check that stack isn't empty before popping last one off (deleting last entry)
+// Main function compares the two processed strings for equality
