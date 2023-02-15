385adff25a7601584833e70997f32684e17bb40d
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-02-15 17:10:12 -0500

    Time: 71 ms (73.77%), Space: 44 MB (51.47%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let stack = []; // monotonic increasing
+    let answer = new Array(nums1.length).fill(-1);
+    let hash = {}; // hashmap will store nums in nums2 and their indices
+    
+    // populate hashmap
+    for (let i = 0; i < nums1.length; i++) {
+        hash[nums1[i]] = i;
+    }
+    
+    for (let i = 0; i < nums2.length; i++) {
+        while (stack.length && nums2[i] > stack[stack.length - 1]) {
+            const stackEnd = stack[stack.length - 1];
+            const index = hash[stackEnd];
+            
+            if (index !== undefined) {
+                answer[index] = nums2[i];
+            }
+            
+            stack.pop();
+        }
+        
+        stack.push(nums2[i]);
+    }
+    
+    return answer;
+};
+
+// 
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
