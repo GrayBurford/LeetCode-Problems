0e186a7e655322f2a3336cb46683e05b31ead9fb
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-02-28 17:09:25 -0500

    Time: 107 ms (92.26%), Space: 50.4 MB (23.90%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -39,8 +39,11 @@ var insertIntoBST = function(root, val) {
     return root;
 };
 
+// base case, if no root passed in, create new TreeNode with input val and return
 // Instantiate dfs helper function with binary search tree properties for recursion
 // Check target value against root value
 // If smaller, check if root.left exist. If not, set value as root.left
 // If larger, check if root.right exists. If not, set value as root.right
-// If root.left or right exists after logic check, re-call dfs with current node
+// If root.left or right exists after logic check, re-call dfs with root.left or root.right and val (until leaf node is reached without a valid child for input val, then val will be set to that child)
+// call initial dfs function with BST root and val
+// problem calls for us to return original root
