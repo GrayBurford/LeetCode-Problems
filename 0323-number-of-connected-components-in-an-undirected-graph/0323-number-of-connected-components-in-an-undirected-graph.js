ce609a36c400e951e9b40fff7cf541bb06f22ff6
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-03-03 14:46:12 -0500

    Time: 75 ms (84.89%), Space: 48.2 MB (17.11%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.js
@@ -40,3 +40,11 @@ var countComponents = function(n, edges) {
     
     return answer;
 };
+
+// Need answer/components variable
+// Start a seen array to log when we have visited a node
+// Build graph from input info: instantiate map, set each node to empty array, loop over edges input and push neighbors into hash map
+// dfs function to traverse neighbors from given node. Loop over a node's neighbors, check if that neighbor has already been seen/visited, and if not, set seen[neighbor] to be visited/true, then recursively call dfs on neighbor
+// Where do we start our dfs? How do we know which nodes haven't been visited?
+// Start dfs everywhere at the same time -- loop over all n nodes, checking first if node has been visited, and if not increment components answer and call dfs on that node
+// return answer
