e47f57699a3079cf8f520f2421e210ee95d4bcf3
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-03-03 11:18:12 -0500

    Time: 65 ms (76.50%), Space: 44.5 MB (30.29%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {    
+    let seen = new Set([0]);
+    
+    // EX: rooms = [[1],[2],[3],[]]
+    function dfs(node) {
+        for (const neighbor of rooms[node]) {
+            if (!seen.has(neighbor)) {
+                seen.add(neighbor);
+                dfs(neighbor);
+            }
+        }
+    }
+    
+    dfs(0);
+    
+    return rooms.length > seen.size ? false : true;
+    
+};
+// rooms[i] is the set of keys that you get from visiting room i. room[0] is room/node 0, which will give you an array of keys to other rooms
+// rooms[i] is an array of other rooms we can visit from the current room, which makes this graph given as an adjacency list
+// Start at room 0, need to visit every room.
+// At every node i, the neighbors are rooms[i] -> [...]
+// Start a dfs at node/room 0 and visit every node, then true. If we can't visit all nodes, then false.
+// Check by adding visited nodes to set seen. If size of seen never reaches length of input (num rooms), then we know we didn't visit every room (some keys couldn't be reached)
