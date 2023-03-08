59952f3aab26bb3fe1c670e35e7a13431d52de1e
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-03-08 12:39:06 -0500

    Time: 62 ms (97.87%), Space: 45.7 MB (18.09%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function(root, target, k) {
+    function dfs(node, parent) {
+        if (!node) return;
+        
+        node.parent = parent;
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+    
+    dfs(root, null);
+    
+    let queue = [target];
+    let seen = new Set([target]);
+    let distance = 0;
+    
+    while (queue.length && distance < k) {
+        let currLen = queue.length;
+        let nextQueue = [];
+        
+        for (let i = 0; i < currLen; i++) {
+            let node = queue[i];
+            
+            for (const neighbor of [node.left, node.right, node.parent]) {
+                if (neighbor && !seen.has(neighbor)) {
+                    seen.add(neighbor);
+                    nextQueue.push(neighbor);
+                }
+            }
+        }
+        
+        queue = nextQueue;
+        distance++;
+    }
+    
+    return queue.map(node => node.val);
+};
+// Both the DFS and BFS perform constant work at each node, and only visit each node at most once. Therefore we have a time and space complexity of O(n) (the space comes from the recursion call stack when we assign the parent pointers, the queue, and seen.
