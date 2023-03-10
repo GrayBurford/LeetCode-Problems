3c4f77f4c774ff64670d3ff60c9a86b625da79bb
GrayB <90840369+GrayBurford@users.noreply.github.com>
2023-03-10 14:20:52 -0500

    Time: 71 ms, Memory: 51.1 MB - LeetHub

--- jump-game-iii/jump-game-iii.js
+++ jump-game-iii/jump-game-iii.js
@@ -3,26 +3,6 @@
 //  * @param {number} start
 //  * @return {boolean}
 //  */
-// var canReach = function(arr, start) {
-//     let seen = new Map();
-//     seen.set(start, true);
-    
-//     function bfs(i) {
-//         if (i < 0 || i > arr.length - 1) return false;
-//         if (seen.get(i)) return false;
-//         if (arr[i] === 0) return true;
-
-//         seen.set(i, true);
-
-//         return bfs(i + arr[i]) || bfs(i - arr[i]);
-//     }
-    
-//     if (bfs(start + arr[start]) || bfs(start - arr[start])) {
-//         return true;
-//     }
-    
-//     return false;
-// };
 
 function canReach(arr, start) {
     let seen = [];   
@@ -41,3 +21,8 @@ function canReach(arr, start) {
     
     return false;
 };
+
+// declare seen variable
+// recursively call a bfs on starting position's 2 posible moves
+// bfs will check that the new index is in bounds of the array, that it hasn't already been visited, and if the value is 0
+// If in bounds, not seen, and not target value, mark those new indices as "seen" with -1, and return recursive call on those indices 2 moves
