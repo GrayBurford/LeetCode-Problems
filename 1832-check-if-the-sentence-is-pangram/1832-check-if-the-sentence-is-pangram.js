1f663aa8e4d59c649f94223b89fcca56ac109b36
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-11 12:20:29 -0500

    Update 1832-check-if-the-sentence-is-pangram.js

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -2,14 +2,28 @@
  * @param {string} sentence
  * @return {boolean}
  */
-var checkIfPangram = function(sentence) {
-    let set = new Set(sentence);
+// var checkIfPangram = function(sentence) {
+//     let set = new Set(sentence);
     
-    for (let char of 'abcdefghijklmnopqrstuvwxyz') {
-        if (!set.has(char)) {
-            return false;
+//     for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+//         if (!set.has(char)) {
+//             return false;
+//         }
+//     }
+    
+//     return true;
+// };
+
+var checkIfPangram = function(sentence) {
+    let seen = new Map();
+    
+    for (let char of sentence) {
+        if (seen.has(char)) {
+            seen.set(char, seen.get(char) + 1);
+        } else {
+            seen.set(char, 1);
         }
     }
     
-    return true;
+    return seen.size === 26;
 };
