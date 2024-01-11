be0d08aea6e4a5b1e7cfd7316a77255a1075c3b8
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-11 12:22:14 -0500

    Update 1133-largest-unique-number.js

--- 1133-largest-unique-number/1133-largest-unique-number.js
+++ 1133-largest-unique-number/1133-largest-unique-number.js
@@ -2,26 +2,48 @@
  * @param {number[]} nums
  * @return {number}
  */
-var largestUniqueNumber = function(nums) {
-    let count = new Map();
-    let ans = [];
+var largestUniqueNumber = function(nums) {    
+    let ans = 0;    
+    let freqMap = new Map();
     
     for (let num of nums) {
-        count.set(num, (count.get(num) || 0) + 1);
+        freqMap.set(num, (freqMap.get(num) || 0) + 1)
     }
-    console.log(count);
     
-    for (const [k, v] of count) {
-        if (v === 1) {
-            ans.push(k);
+    for (const [key, val] of freqMap) {
+        if (val === 1) {
+            ans = Math.max(ans, key);
         }
-    }   
+    }
     
-    return !ans.length ? -1 : Math.max(...ans);
+    return ans === 0
+        ? -1
+        : ans;
+    
 };
+
+
+
 
 // initialize hash map
 // fill map: map keys to each num with value being that num's frequency
 // loop over map for keys whose values are 1
 // push those keys into an answer array
 // Math.max(...ans)
+
+
+
+//     let freqMap = new Map();
+//     let ans = [];
+    
+//     for (let num of nums) {
+//         freqMap.set(num, (freqMap.get(num) || 0) + 1);
+//     }
+    
+//     for (const [k, v] of freqMap) {
+//         if (v === 1) {
+//             ans.push(k);
+//         }
+//     }   
+    
+//     return !ans.length ? -1 : Math.max(...ans);
