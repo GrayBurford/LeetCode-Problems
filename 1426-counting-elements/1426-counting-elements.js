4786024b8a4a99260a670aa212da1c2ecb286bd1
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-11 12:19:00 -0500

    Update 1426-counting-elements.js

--- 1426-counting-elements/1426-counting-elements.js
+++ 1426-counting-elements/1426-counting-elements.js
@@ -2,15 +2,28 @@
  * @param {number[]} arr
  * @return {number}
  */
-var countElements = function(arr) {
-    let set = new Set(arr);
-    let out = 0;
+// var countElements = function(arr) {
+//     let set = new Set(arr);
+//     let out = 0;
     
-    for (let i = 0; i < arr.length; i++) {
-        if (set.has(arr[i]+1)) {
-            out++;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (set.has(arr[i]+1)) {
+//             out++;
+//         }
+//     }
+    
+//     return out;
+// };
+
+var countElements = function(arr) {
+    let seen = new Set(arr);
+    let ans = 0;
+    
+    for (let num of arr) {
+        if (seen.has(num+1)) {
+            ans++;
         }
     }
     
-    return out;
+    return ans;
 };
