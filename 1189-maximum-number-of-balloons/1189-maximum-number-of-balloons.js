1f269925a697437dcd06b543eaa96005753a0178
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-11 12:23:21 -0500

    Update 1189-maximum-number-of-balloons.js

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -4,36 +4,35 @@
  */
 var maxNumberOfBalloons = function(text) {
 
-    let counts = new Map();
-    let output = 0;
-    const balloon = "balloon";
+    let freqMap = new Map();
+    let ans = 0;
     
     for (let char of text) {
-        counts.set(char, (counts.get(char) || 0) + 1);
+        freqMap.set(char, (freqMap.get(char) || 0) + 1);
     }
     
-    while (counts.size) {
-        for (char of balloon) {
-            if (counts.get(char) === undefined) {
-                return output;
+    while (freqMap.size) {
+        for (const char of "balloon") {
+            if (freqMap.get(char) === undefined) {
+                return ans;
             }
             
-            counts.set(char, (counts.get(char) - 1));
+            freqMap.set(char, (freqMap.get(char) - 1));
             
-            if (counts.get(char) === 0) {
-                counts.delete(char);
+            if (freqMap.get(char) === 0) {
+                freqMap.delete(char);
             }
         } 
         
-        output++;
+        ans++;
     }
 
-    return output;
+    return ans;
 };
 
 // - Loop over text and add each character to a hash map that tracks the characters frequency (count)
 // - Start while loop for the size of hash map
 // - Loop over characters in balloon, and remove 1 count from each character in hash map for each occurrence
-// - If char is undefined (doesn't exist), then return output count
+// - If char is undefined (doesn't exist), then return ans count
 // - If character does exist and we subtract 1 count, also check if count is now 0, so we can then delete char from map
-// - After for loop, output++ (counts number of times balloon can be successfully spelled)
+// - After for loop, ans++ (counts number of times balloon can be successfully spelled)
