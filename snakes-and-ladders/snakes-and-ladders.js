bd652577a2f619e9dfb74a2fef4ebb2fefe80a29
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-12 17:56:10 -0500

    Time: 66 ms, Memory: 46.9 MB - LeetHub

--- snakes-and-ladders/snakes-and-ladders.js
+++ snakes-and-ladders/snakes-and-ladders.js
@@ -6,13 +6,16 @@ var snakesAndLadders = function (board) {
     const n = board.length;
 
     board.reverse();
+    
     function getCoordinates(square) {
         const sq = square - 1;
         const r = Math.floor(sq / n);
         let c = (sq % n);
+        
         if (r % 2) {
             c = n - (sq % n) - 1;
         }
+        
         return [r, c];
     }
 
@@ -20,23 +23,31 @@ var snakesAndLadders = function (board) {
         const queue = [[1, 0]];
         const visit = new Set();
         const target = (n ** 2);
+        
         while (queue.length > 0) {
             const [square, moves] = queue.shift();
 
             for (let i = 1; i < 7; i++) {
                 let nextSquare = square + i;
                 const [r, c] = getCoordinates(nextSquare);
+                
                 if (board[r][c] !== -1) {
                     nextSquare = board[r][c];
                 }
-                if (nextSquare === target) return moves + 1;
+                
+                if (nextSquare === target) {
+                    return moves + 1;
+                }
+                
                 if (!visit.has(nextSquare)) {
                     visit.add(nextSquare);
                     queue.push([nextSquare, moves + 1]);
                 }
             }
         }
+        
         return -1;
     }
+    
     return bfs();
 };
