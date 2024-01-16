7628e1f8d372692d5005d3e524610ab85a4a3242
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-01-16 13:51:28 -0500

    Time: 782 ms (95.06%), Space: 32.1 MB (24.71%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.py
@@ -14,3 +14,11 @@ class Solution:
             answer = answer + 1
         
         return answer
+    
+    
+# initialize sum of list, target (half of list sum), and answer variable
+# initialize max heap from list. With python (min heap), must convert vals to negative for max heap
+# start while loop for target > 0, so we only remove values until half the list is decremented
+# pop largest num. Half this num and subtract from target. Push this halved num back onto heap (pop 42, add 21 back)
+# increment answer (num of operations)
+# once while loop exits, we have decremented list past half of the total sum and can return answer (min num of operations to half a list)
