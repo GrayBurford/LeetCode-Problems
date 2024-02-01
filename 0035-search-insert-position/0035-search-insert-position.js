be1b31228e6e154773a56573068533d259cb5ec6
GrayB <90840369+GrayBurford@users.noreply.github.com>
2024-02-01 12:19:51 -0500

    Time: 46 ms (88.24%), Space: 48.7 MB (16.63%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,27 +4,36 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (target < nums[0]) return 0;
-    if (target > nums[nums.length-1]) return nums.length;
-
-    let start = 0;
-    let end = nums.length - 1;
-
-    while (start <= end) {
-        let mid = Math.floor((start+end)/2);
-        if (target === nums[start]) return start;
-        if (target === nums[end]) return end;
-        if (target === nums[mid]) return mid;
-    
-        if (target < nums[mid]) {
-            end = mid-1;
-            if (target > nums[end]) return end+1;
-        }
-
-        if (target > nums[mid]) {
-            start = mid+1;
-            if (target < nums[start]) return start;
-        }
+    if (nums.length === 1) {
+        if (target === nums[0]) return 0;
+        return target > nums[0] ? 1 : 0;
     }
     
+    if (target < nums[0]) return 0;
+    if (target > nums[nums.length - 1]) return nums.length;
+    
+    let left = 0;
+    let right = nums.length - 1;
+    let mid;
+    
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (target < nums[mid]) {
+            right = mid - 1;
+        }
+        if (target > nums[mid]) {
+            left = mid + 1;
+        }
+        if (target === nums[mid]) {
+            return mid;
+        }        
+    }
+    
+    return left;
 };
+
+// SORTED is key in this problem, which means we can use binary search
+// instantiate left and right pointers
+// while loop while left <= right
+// check if target is less than or greater than mid (or equal)
+// if not found, reset right or left bounds to meet new range of possible values
